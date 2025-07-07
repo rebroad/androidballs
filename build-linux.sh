dcd9b5185b999bbaddec5f283ba7b76a50b27add
R E Broadley <rebroad@users.noreply.github.com>
2025-07-07 18:01:47 +0100

    updates

--- build-linux.sh
+++ build-linux.sh
@@ -3,30 +3,48 @@
 
 set -e
 
-echo "Building SDL2 for Linux..."
+echo "Checking for system-wide SDL2..."
 
-# Create build directory
-mkdir -p build-linux
-cd build-linux
+# Check if SDL2 is available system-wide
+if pkg-config --exists sdl2; then
+    echo "SDL2 found system-wide, using it..."
+    SDL2_CFLAGS=$(pkg-config --cflags sdl2)
+    SDL2_LIBS=$(pkg-config --libs sdl2)
+    USE_SYSTEM_SDL2=true
+else
+    echo "SDL2 not found system-wide, building from source..."
+    USE_SYSTEM_SDL2=false
+    
+    # Create build directory
+    mkdir -p build-linux
+    cd build-linux
 
-# Configure SDL2
-../../SDL/configure --prefix=$(pwd)/install --enable-shared=no --enable-static=yes
+    # Configure SDL2
+    ../../SDL/configure --prefix=$(pwd)/install --enable-shared=no --enable-static=yes
 
-# Build SDL2
-make -j$(nproc)
+    # Build SDL2
+    make -j$(nproc)
 
-# Install SDL2 to our build directory
-make install
-
-cd ..
+    # Install SDL2 to our build directory
+    make install
+    
+    # Install SDL2 system-wide
+    echo "Installing SDL2 system-wide..."
+    sudo cp -r install/* /usr/local/
+    sudo ldconfig
+    
+    cd ..
+    
+    SDL2_CFLAGS="-I/usr/local/include/SDL2"
+    SDL2_LIBS="-L/usr/local/lib -lSDL2"
+fi
 
 echo "Building Hello World app..."
 
-# Build our app with the compiled SDL2
+# Build our app
 gcc -Wall -Wextra -std=c99 -O2 \
-    -Ibuild-linux/install/include/SDL2 \
-    -Lbuild-linux/install/lib \
+    $SDL2_CFLAGS \
     -o helloworld main.c \
-    -lSDL2 -lm -lpthread -ldl
+    $SDL2_LIBS -lm -lpthread -ldl
 
 echo "Build complete! Run with: ./helloworld" 
--- build-windows.sh
+++ build-windows.sh
@@ -11,27 +11,55 @@ export AR=x86_64-w64-mingw32-ar
 export RANLIB=x86_64-w64-mingw32-ranlib
 export WINDRES=x86_64-w64-mingw32-windres
 
-# Create build directory
-mkdir -p build-windows
-cd build-windows
+# Check if Windows SDL2 is available system-wide
+WINDOWS_SDL2_DIR="/usr/local/x86_64-w64-mingw32"
+if [ -d "$WINDOWS_SDL2_DIR/include/SDL2" ] && [ -d "$WINDOWS_SDL2_DIR/lib" ]; then
+    echo "Windows SDL2 found system-wide, using it..."
+    SDL2_CFLAGS="-I$WINDOWS_SDL2_DIR/include/SDL2"
+    SDL2_LIBS="-L$WINDOWS_SDL2_DIR/lib -lSDL2"
+    USE_SYSTEM_SDL2=true
+else
+    echo "Windows SDL2 not found system-wide, building from source..."
+    USE_SYSTEM_SDL2=false
+    
+    # Create build directory
+    mkdir -p build-windows
+    cd build-windows
 
-# Configure SDL2 for Windows
-../SDL/configure \
-    --host=x86_64-w64-mingw32 \
-    --prefix=$(pwd)/install \
-    --enable-shared \
-    --enable-static \
-    --disable-video-vulkan \
-    --disable-video-opengl \
-    --disable-video-opengles \
-    --disable-video-opengles2
+    # Configure SDL2 for Windows
+    ../SDL/configure \
+        --host=x86_64-w64-mingw32 \
+        --prefix=$(pwd)/install \
+        --enable-shared \
+        --enable-static \
+        --disable-video-vulkan \
+        --disable-video-opengl \
+        --disable-video-opengles \
+        --disable-video-opengles2
 
-# Build SDL2
-make -j$(nproc)
+    # Build SDL2
+    make -j$(nproc)
 
-# Install SDL2
-make install
+    # Install SDL2 to our build directory
+    make install
+    
+    # Install SDL2 system-wide for Windows cross-compilation
+    echo "Installing Windows SDL2 system-wide..."
+    sudo mkdir -p $WINDOWS_SDL2_DIR
+    sudo cp -r install/* $WINDOWS_SDL2_DIR/
+    
+    cd ..
+    
+    SDL2_CFLAGS="-I$WINDOWS_SDL2_DIR/include/SDL2"
+    SDL2_LIBS="-L$WINDOWS_SDL2_DIR/lib -lSDL2"
+fi
 
-echo "SDL2 for Windows built successfully!"
-echo "Headers: $(pwd)/install/include/SDL2/"
-echo "Libraries: $(pwd)/install/lib/" 
+echo "Building Windows Hello World executable..."
+
+# Build our app for Windows
+x86_64-w64-mingw32-gcc -Wall -Wextra -std=c99 -O2 \
+    $SDL2_CFLAGS \
+    -o helloworld.exe main.c \
+    $SDL2_LIBS -lm
+
+echo "Windows build complete! Executable: helloworld.exe" 
